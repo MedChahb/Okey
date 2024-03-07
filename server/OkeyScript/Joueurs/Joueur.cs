a1047a4356d463d5331ddbb23a222e8a9c6476bb
Goktekin Muhammed Emin <muhammed-emin.goktekin@etu.unistra.fr>
2024-03-07 16:54:54 +0100

    GL-4: te

--- server/OkeyScript/Joueurs/Joueur.cs
+++ server/OkeyScript/Joueurs/Joueur.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,17 +8,19 @@ using Okey.Tuiles;
 namespace Okey.Joueurs
 {
     public abstract class Joueur
-    { 
+    {
         protected int id;
         protected String Name;
         protected List<List<Tuile>> chevalet = new List<List<Tuile>>(); // 15tuile + 1case vide
         public Boolean Tour;
         protected Boolean Gagnant;
-        
 
-        public Joueur(int id, String Name) {
-            this.id = id; this.Name = Name; 
-            this.Gagnant = false; this.Tour = false;
+        public Joueur(int id, String Name)
+        {
+            this.id = id;
+            this.Name = Name;
+            this.Gagnant = false;
+            this.Tour = false;
 
             //initialisation du chevalet (ready to get Tuiles)
             for (int i = 0; i < 2; i++)
@@ -30,50 +32,76 @@ namespace Okey.Joueurs
                     this.chevalet[i].Add(null);
                 }
             }
-
         }
 
         public abstract void Gagne();
 
         public void VerifChevalet()
         {
-            //chech for series then 
+            //chech for series then
             this.Gagne();
         }
 
         public void Piocher() { }
+
         public void JeterTuile(Tuile T) { }
 
-        public void PassTour() 
+        public void PassTour()
         {
             this.Tour = false;
         }
+
         public void EstTour()
         {
             this.Tour = true;
         }
 
-        public void AjoutToChevalet(Tuile t)
+        public void EstPlusTour()
         {
-
+            this.Tour = false;
         }
 
-        public List<List<Tuile>> GetChevalet() { return this.chevalet; }
+        public void AjoutToChevalet(Tuile t) { }
 
-        public void Ajouer() { this.Tour=true; }
+        public List<List<Tuile>> GetChevalet()
+        {
+            return this.chevalet;
+        }
+
+        public void Ajouer()
+        {
+            this.Tour = true;
+        }
+
         public void AjoutTuileChevalet(Tuile t)
         {
-            for(int j = 0; j < 2;j++) { 
-                for(int i=0; i < 8; i++)
+            for (int j = 0; j < 2; j++)
+            {
+                for (int i = 0; i < 8; i++)
                 {
                     if (this.chevalet[j][i] == null)
                     {
-                        this.chevalet[j][i] = t; 
+                        this.chevalet[j][i] = t;
                         return;
                     }
                 }
             }
         }
-        public override abstract String ToString();
+
+        public void EnvoyerMessage(string message)
+        {
+            // Utilisez ici votre mécanisme réel de communication avec le client Unity (WebSocket, HTTP, etc.)
+            Console.WriteLine($"Envoi du message au joueur {id}: {message}");
+            // Exemple avec WebSocket :
+            // webSocketConnection.Send(message);
+        }
+
+        public void EnvoyerMessageTour(bool TourActuel)
+        {
+            string message = TourActuel ? "C'est votre tour." : "Ce n'est pas votre tour.";
+            EnvoyerMessage(message);
+        }
+
+        public abstract override String ToString();
     }
 }
