33c4b83c8d86b154e67da1de4274bab9ab9f5c5b
CHAHBAOUI mohammed <mohammed.chahbaoui@etu.unistra.fr>
2024-03-15 15:05:10 +0100

    Gl-4: ajout d'une classe Coords.
    pour gerer les Couples (y,x) des tuiles de chevalet.

---
+++ server/OkeyScript/Coord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Okey
+{
+    public class Coord
+    {
+        private int x;
+        private int y;
+
+        public Coord(int y, int x)
+        {
+            this.y = y;
+            this.x = x;
+        }
+
+        public int getX() { return x; }
+        public int getY() { return y; }
+
+    }
+}
