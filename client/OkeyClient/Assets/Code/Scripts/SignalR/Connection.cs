bb7e517d2fddb061b990158df10e029b835ca778
ERKEN EFE <efe.erken@etu.unistra.fr>
2024-04-10 23:30:25 +0200

    Add SignalR Client 8.0.4 netstandard2.1 DLLs to Unity project

--- client/OkeyClient/Assets/Code/Scripts/SignalR/Connection.cs
+++ client/OkeyClient/Assets/Code/Scripts/SignalR/Connection.cs
@@ -1,49 +1,49 @@
-//using System;
-//using Microsoft.AspNetCore.SignalR.Client;
-//using UnityEngine;
+using System;
+using Microsoft.AspNetCore.SignalR.Client;
+using UnityEngine;
 
-//public class SignalRConnector : MonoBehaviour
-//{
-//    private HubConnection hubConnection;
+public class SignalRConnector : MonoBehaviour
+{
+    private HubConnection hubConnection;
 
-//    async void Start()
-//    {
-//        hubConnection = new HubConnectionBuilder().WithUrl("http://localhost:5105/hub").Build();
+    private async void Start()
+    {
+        hubConnection = new HubConnectionBuilder().WithUrl("http://localhost:5105/hub").Build();
 
-//        // Setup event listeners, if any
-//        ConfigureHubEvents();
+        // Setup event listeners, if any
+        ConfigureHubEvents();
 
-//        // Start the connection
-//        try
-//        {
-//            await hubConnection.StartAsync();
-//            Debug.Log("SignalR connection started.");
-//        }
-//        catch (Exception ex)
-//        {
-//            Debug.LogError($"SignalR connection failed: {ex.Message}");
-//        }
-//    }
+        // Start the connection
+        try
+        {
+            await hubConnection.StartAsync();
+            Debug.Log("SignalR connection started.");
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"SignalR connection failed: {ex.Message}");
+        }
+    }
 
-//    private void ConfigureHubEvents()
-//    {
-//        // Example: Listen for a message from the server
-//        hubConnection.On<string>(
-//            "ReceiveMessage",
-//            (message) =>
-//            {
-//                Debug.Log($"Message received from server: {message}");
-//            }
-//        );
-//    }
+    private void ConfigureHubEvents()
+    {
+        // Example: Listen for a message from the server
+        hubConnection.On<string>(
+            "ReceiveMessage",
+            (message) =>
+            {
+                Debug.Log($"Message received from server: {message}");
+            }
+        );
+    }
 
-//    async void OnDestroy()
-//    {
-//        // Stop the connection when the GameObject is destroyed
-//        if (hubConnection != null)
-//        {
-//            await hubConnection.StopAsync();
-//            await hubConnection.DisposeAsync();
-//        }
-//    }
-//}
+    private async void OnDestroy()
+    {
+        // Stop the connection when the GameObject is destroyed
+        if (hubConnection != null)
+        {
+            await hubConnection.StopAsync();
+            await hubConnection.DisposeAsync();
+        }
+    }
+}
