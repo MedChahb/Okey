678d7763e40f6960ca14c9560d36c950d35a0840
EL MOUSSAOUI MEHDI <me.el-moussaoui@etu.unistra.fr>
2024-05-09 17:24:09 +0200

    GL-23: Creation d'une premiere landpage

---
+++ server/okeywebsite/src/app/FallingTile.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './FallingTile.module.css';
+
+const FallingTile = () => {
+  return <div className={styles.tile}></div>;
+};
+
+export default FallingTile;
--- server/okeywebsite/tailwind.config.ts
+++ server/okeywebsite/tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primaryColor: '#E3E3BB',
+        primaryColorHover: '#d1d1ae'
+      }
     },
   },
   plugins: [],
