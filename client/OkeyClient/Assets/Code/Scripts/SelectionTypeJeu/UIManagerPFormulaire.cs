c11d458bfde944493713850b76379f2451f7e24a
Thibault GRIVEL <thibault.grivel@etu.unistra.fr>
2024-05-11 17:49:30 +0100

    Remise en commentaire d'une ligne de code

--- client/OkeyClient/Assets/Code/Scripts/SelectionTypeJeu/UIManagerPFormulaire.cs
+++ client/OkeyClient/Assets/Code/Scripts/SelectionTypeJeu/UIManagerPFormulaire.cs
@@ -90,7 +90,7 @@ public class UIManagerPFormulaire : MonoBehaviour
 
     public void onPublicLobbyClicked()
     {
-        lobbyPlayerWaiting.SetActive(true);
+        // lobbyPlayerWaiting.SetActive(true);
         onJoinLobbyButtonPressed();
     }
 
