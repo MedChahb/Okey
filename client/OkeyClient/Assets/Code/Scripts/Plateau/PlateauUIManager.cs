ee94408c430c029345cfdf14b6bd3e707f1a58d6
ELLEITHY HUSSEIN <hussein.el-leithy@etu.unistra.fr>
2024-05-15 00:27:21 +0200

    avatar object in lobby created needs assignment

---
+++ client/OkeyClient/Assets/Code/Scripts/Plateau/PlateauUIManager.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlateauUIManager : MonoBehaviour
+{
+    // public static UIManagerPFormulaire Instance { get; private set; }
+
+    public GameObject playerAvatars;
+}
